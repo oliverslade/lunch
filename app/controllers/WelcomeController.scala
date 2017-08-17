567d8114bbfefd1bc7abf55e260fe4ecfa9cafce
oliver.slade <oliver.slade@digital.hmrc.gov.uk>
2017-08-17 11:45:26 +0100

    Created welcome page: - Create TDD tests - Added welcome to the routes - Created welcome controller with index method - Created welcome view

---
+++ app/controllers/WelcomeController.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import play.api.mvc._
+
+/**
+  * Created by oliverslade on 17/08/17.
+  */
+class WelcomeController extends Controller{
+  def index = Action { implicit request =>
+  Ok(views.html.welcome())
+  }
+}
